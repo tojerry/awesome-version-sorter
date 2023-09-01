3c57a996da86811731f1763582951b945b4f16c0
Jerry Chang <tojerry@gmail.com>
2023-09-01 15:32:21 -0700

    Update example.js

--- example.js
+++ example.js
@@ -7,5 +7,18 @@ function versionSorter(verionList, isDescending){
   }
 }
 
-var arr4 = ['ISE 3.1 and above (API v1)', 'ISE 1.x and below (API v0)', 'ISE 2.6', 'ISE 3.0', 'ISE 2.7'];
+var arr4 = [
+'ISE 3.1 and above (API v1)', 
+'ISE 1.x and below (API v0)', 
+'ISE 2.6', 
+'ISE 2.7.2',
+'ISE 1.2',
+'ISE 0.1', 
+'ISE 0.2', 
+'ISE 1.0.1',
+'ISE 2.7.x',
+'ISE 1.0.0', 
+'ISE 3.0', 
+'ISE 2.7.1'  
+];
 printLine(versionSorter(arr4, true));
